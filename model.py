cc475d5bb6604ca08e71498436e13ffa92be7586
Kaitlyn Kinerk <ksorum@uw.edu>
2018-03-08 17:11:18 -0800

    NB classifier now classifies 100 testing tweets. Probability of tweet being real or fake is printed.

--- model.py
+++ model.py
@@ -2,14 +2,15 @@ import nltk
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 import random
+import pickle
 
 
 fake_tweets = open('cleaned_tweets/fake_tweets.txt', 'r')
 real_tweets = open('cleaned_tweets/trusted_tweets.txt', 'r')
 
 # categorize tweets
-categorized_tweets = [(tweet, 'fake') for tweet in fake_tweets.readlines()] + \
-            [(tweet, 'real') for tweet in real_tweets.readlines()]
+categorized_tweets = [((list(word_tokenize(tweet))), 'fake') for tweet in fake_tweets.readlines()] + \
+            [((list(word_tokenize(tweet))), 'real') for tweet in real_tweets.readlines()]
 
 random.shuffle(categorized_tweets)
 
@@ -17,6 +18,7 @@ fake_tweets.seek(0)
 real_tweets.seek(0)
 
 all_words = []
+
 
 # ignore uninteresting words
 stopwords = set(stopwords.words('english'))
@@ -36,17 +38,13 @@ fake_tweets.close()
 real_tweets.close()
 
 # find most frequent words longer than 1 character
-freq_dist = nltk.FreqDist(all_words).most_common(3000)
-
-word_features = []
-for w in freq_dist:
-    if len(w[0]) > 1:
-        word_features.append(w[0])
+all_words = nltk.FreqDist(all_words)
+word_features = list(all_words.keys())[:3500]
 
 
 # determine if a frequent word is in the given file
 def find_features(tweet):
-    words = set(word_tokenize(tweet))
+    words = set(tweet)
     features = {}
     for w in word_features:
         features[w] = (w in words)
@@ -54,36 +52,31 @@ def find_features(tweet):
     return features
 
 
-# build the training and testing data
-feature_sets = [(find_features(tweet), category)
-                for (tweet, category) in categorized_tweets]
+training_set = categorized_tweets[:5505]
+testing_set = categorized_tweets[5505:]
 
-training_set = feature_sets[:2750]
-testing_set = feature_sets[2750:-100]
+training_features = [(find_features(tweet), category) for (tweet, category) in training_set] 
+testing_features = [(find_features(tweet), category) for (tweet, category) in testing_set] 
+
 
 # run the data sets through the NaiveBayes classifier and print results
-classifier = nltk.NaiveBayesClassifier.train(training_set)
+classifier = nltk.NaiveBayesClassifier.train(training_features)
 
-#print("Classifier accuracy percent:",
-#      (nltk.classify.accuracy(classifier, testing_set))*100)
+print("Classifier accuracy percent:",
+      (nltk.classify.accuracy(classifier, testing_features))*100)
 
-#classifier.show_most_informative_features(30)
+# classifier.show_most_informative_features(30)
 
 
 # give each tweet an individual score - work in progress
-
-tagged_test_set = feature_sets[-100:]
-
-print(training_set[0:5])
-print(testing_set[0:5])
-print(tagged_test_set[0:5])
-
 tagged_tweets = []
 
-for tweet in tagged_test_set:
-    text = " ".join(tweet[0].keys())
-    auth = classifier.classify(find_features(text))
-    print(text, auth)
-    #tagged_tweets.append(tuple([text, auth]))
+labels = ['real', 'fake']
 
-#print(tagged_tweets)
+for (tweet, category) in testing_set:
+    text = " ".join(tweet)
+    auth = classifier.prob_classify(find_features(tweet))
+    print(text + '\n\t\tPROB REAL: ' + str("%.2f" % (auth.prob('real') * 100)) + ' PROB FAKE: ' + str("%.2f" % (auth.prob('fake') * 100)))
+    print()
+    tagged_tweets.append(tuple([text, auth]))
+
